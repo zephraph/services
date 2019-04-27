1d3022ae69ab7fb2b358b270120f15d21554dcb2
Zephraph <zephraph@gmail.com>
2019-04-26 20:22:33 -0400

    Initial setup

---
+++ src/api/circleci.ts
@@ -0,0 +1,137 @@
+// Module
+import axios from "axios";
+import { Artifact, BuildSummary, CircleCI, CircleCIOptions, GitType, BuildWithSteps } from "circleci-api";
+
+import { safelyFetchEnvs } from "../utils/env";
+
+const { CIRCLE_TOKEN } = safelyFetchEnvs(["CIRCLE_TOKEN"]);
+
+interface BuildSummaryExtended extends BuildSummary {
+  workflows?: {
+    job_name: string;
+  };
+}
+
+interface PullRequest {
+  head_sha: string;
+  url: string;
+}
+
+interface BuildWithStepsExtended extends BuildWithSteps {
+  pull_requests: PullRequest[];
+}
+
+interface FetchArtifactParams {
+  jobName: string;
+  artifactName: string;
+  buildNum?: number;
+  branchName?: string;
+}
+
+export class Circle {
+  private owner: string;
+  private repo: string;
+  private api: CircleCI;
+
+  constructor({ owner, repo }: { owner: string; repo: string }) {
+    this.owner = owner;
+    this.repo = repo;
+
+    // Configure the factory with some defaults
+    const options: CircleCIOptions = {
+      // Required for all requests
+      token: CIRCLE_TOKEN, // Set your CircleCi API token
+
+      // Git information is required for project/build/etc endpoints
+      vcs: {
+        type: GitType.GITHUB, // default: github
+        owner,
+        repo
+      }
+    };
+
+    // Create the api object
+    this.api = new CircleCI(options);
+  }
+
+  async getPullRequest(buildNum: number): Promise<string | null> {
+    let build;
+
+    try {
+      build = (await this.api.build(buildNum)) as BuildWithStepsExtended;
+    } catch (error) {
+      console.error(`Failed to get circle build details for job #${buildNum} of ${this.owner}/${this.repo}`);
+      throw error;
+    }
+
+    if (build.pull_requests.length > 0 && build.branch !== "master") {
+      const pr = build.pull_requests[0].url;
+      console.log("Related pull request:", pr);
+      return pr;
+    }
+
+    return null;
+  }
+
+  /**
+   * Grab the latest artifacts from a successful build on a certain branch
+   * @param [branch="master"] - Artifacts for certain branch
+   * @return List of successfully built artifact objects
+   */
+  async getLatestArtifacts(branch = "master"): Promise<Artifact[]> {
+    try {
+      // Will use the repo defined in the options above
+      const result: Artifact[] = await this.api.latestArtifacts({
+        branch,
+        filter: "successful"
+      });
+      console.log(`Found ${result.length} artifacts`);
+      return result;
+    } catch (error) {
+      console.log("No build artifacts found", error);
+    }
+
+    return [];
+  }
+
+  async fetchArtifact({
+    jobName,
+    artifactName,
+    buildNum,
+    branchName = "master"
+  }: FetchArtifactParams): Promise<string | undefined> {
+    let latestBuild;
+    let artifact;
+
+    if (!buildNum) {
+      try {
+        latestBuild = (await this.api.buildsFor(branchName, {
+          filter: "successful",
+          limit: 10
+        })).filter((build: BuildSummaryExtended) => build.workflows && build.workflows.job_name === jobName)[0];
+      } catch (error) {
+        console.error(`Failed to find latest master ${jobName} job`);
+        throw error;
+      }
+    }
+
+    try {
+      if (buildNum || latestBuild) {
+        let targetArtifact = (await this.api.artifacts(
+          // @ts-ignore
+          buildNum || latestBuild.build_num!
+        )).filter(artifact => artifact.path.includes(artifactName!))[0];
+        if (targetArtifact && targetArtifact.url) {
+          artifact = (await axios.get(targetArtifact.url)).data;
+        }
+      }
+    } catch (error) {
+      const branchText = buildNum ? "" : `latest ${branchName} `;
+      const buildText = buildNum ? `#${buildNum} ` : "";
+      console.error(`Found ${branchText}job ${buildText}but failed to fetch artifact`);
+      throw error;
+    }
+
+    return artifact;
+  }
+}
---
+++ src/api/github.ts
@@ -0,0 +1,140 @@
+import retry from "@octokit/plugin-retry";
+import throttling from "@octokit/plugin-throttling";
+import GitHubApi from "@octokit/rest";
+import { URL } from "url";
+
+import { safelyFetchEnvs } from "../utils/env";
+
+const repo = ({ owner, repo }: Partial<IssueOptions>) => `https://github.com/${owner}/${repo}`;
+
+// @ts-ignore
+const GitHubWithPlugins = GitHubApi.plugin(throttling).plugin(retry);
+
+const { GH_TOKEN } = safelyFetchEnvs(["GH_TOKEN"]);
+
+interface IssueOptions {
+  owner: string;
+  repo: string;
+  number: number;
+}
+
+type GitHubOptions = Partial<IssueOptions> & {
+  dryRun: boolean;
+  owner: string;
+  repo: string;
+  pullRequest: string;
+};
+
+export class GitHub {
+  private readonly _github: GitHubApi;
+  private readonly _issueOpts: IssueOptions;
+
+  constructor({ dryRun, owner, repo, pullRequest }: GitHubOptions) {
+    this._github = new GitHubWithPlugins({
+      auth: `token ${GH_TOKEN}`,
+
+      throttle: {
+        onRateLimit: (retryAfter: number, options: any) => {
+          this._github.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
+
+          // retry three times
+          if (options.request.retryCount < 3) {
+            console.log(`Retrying after ${retryAfter} seconds!`);
+            return true;
+          }
+        },
+        onAbuseLimit: (_retryAfter: number, options: any) => {
+          // does not retry, only logs an error
+          console.error(`Abuse detected for request ${options.method} ${options.url}`);
+        }
+      }
+    });
+
+    this._issueOpts = {
+      owner,
+      repo,
+      number: parseInt(pullRequest.split("/").slice(-1)[0])
+    };
+
+    if (dryRun) {
+      this._github.hook.wrap("request", (req, opts) => {
+        if (opts.method === "GET") {
+          return req(opts);
+        }
+        // tslint:disable-next-line:no-unused
+        const { headers, request, body, method, ...otherOptions } = opts;
+        console.log(`\nAttempting ${method} with options`, otherOptions);
+        if (body) {
+          console.log("body present but omitted for brevity");
+        }
+        return {
+          data: []
+        };
+      });
+    }
+  }
+
+  public async getBaseCommitBuild(buildName: string) {
+    const { data: pr } = await this._github.pulls.get({ ...this._issueOpts });
+
+    if (pr.base.ref !== "master") {
+      console.warn(`${pr.html_url} might not have been branched off of master...`);
+    }
+
+    console.log(`Attempting to get build for ${repo(this._issueOpts)}/commit/${pr.base.sha}`);
+
+    const { data: statuses } = await this._github.repos.listStatusesForRef({
+      owner: this._issueOpts.owner,
+      repo: this._issueOpts.repo,
+      ref: pr.base.sha
+    });
+
+    const build = statuses.find(status => status.context === `ci/circleci: ${buildName}` && status.state === "success");
+
+    if (!build) {
+      console.error("No successful build found for base");
+      throw new Error();
+    }
+
+    console.log(`Build found: ${build.target_url}`);
+
+    return parseInt(new URL(build.target_url).pathname.split("/").pop() as string);
+  }
+
+  public async checkForPRCommentWithString(match: string) {
+    const comments = await this._github.issues.listComments({
+      ...this._issueOpts
+    });
+    return comments.data.find(comment => comment.body.includes(match));
+  }
+
+  public updateComment(comment_id: number, body: string) {
+    return this._github.issues.updateComment({
+      ...this._issueOpts,
+      comment_id,
+      body
+    });
+  }
+
+  public createComment(body: string) {
+    return this._github.issues.createComment({
+      ...this._issueOpts,
+      body
+    });
+  }
+
+  public deleteComment(comment_id: number) {
+    return this._github.issues.deleteComment({
+      ...this._issueOpts,
+      comment_id
+    });
+  }
+
+  public async getAssignees() {
+    let issue = await this._github.issues.get({
+      ...this._issueOpts
+    });
+    let assignees = issue.data.assignees.map(assignee => `@${assignee.login}`);
+    return Array.from(new Set([...assignees, "@zephraph"]));
+  }
+}
